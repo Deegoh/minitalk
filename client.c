aa22b89e5c6e695d5b978625b442aeb4a763ec69
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-01-07 00:30:54 +0100

    send and receive signal between clt-srv

--- client.c
+++ client.c
@@ -6,13 +6,60 @@
 /*   By: tpinto-m <marvin@24lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 14:21:09 by tpinto-m          #+#    #+#             */
-/*   Updated: 2022/01/05 14:22:39 by tpinto-m         ###   ########.fr       */
+/*   Updated: 2022/01/07 00:27:24 by tpinto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+
+static int	ft_isspace(int c)
 {
-	write(1, "Hello clt !", 11);
+	if (c == ' ' || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+static int	ft_atoi(const char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+		res = res * 10 + str[i++] - '0';
+	return (res * sign);
+}
+
+int	main(int ac, char *av[])
+{
+	pid_t	pid;
+
+	if (ac != 3)
+	{
+		write(1, "Missing arg\n", 12);
+		return (1);
+	}
+	pid = ft_atoi(av[1]);
+	printf("pid:%d\n", pid);
+	kill(pid, SIGUSR1);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,26 +6,47 @@
 /*   By: tpinto-m <marvin@24lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 14:22:08 by tpinto-m          #+#    #+#             */
-/*   Updated: 2022/01/05 15:29:48 by tpinto-m         ###   ########.fr       */
+/*   Updated: 2022/01/07 00:08:34 by tpinto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <time.h>
+#include <stdlib.h>
+
+//void	handle_pause(int signum)
+//{
+//
+//}
+
+void	waiter(int signum)
+{
+	write(1, &"waiting connection . . .", 24);
+	printf(" sig:%d\n", signum);
+	pause();
+}
+
+void	catcher(int signum)
+{
+	write(1, &"inside catcher: ", 16);
+	printf("%d\n", signum);
+}
+
+void	timeout(void)
+{
+	write(1, &"Timeout: to slow to enter the pid to client\n", 44);
+}
 
 int	main(void)
 {
-	int		pid;
-	char	input = '\0';
+	int					pid;
+	struct sigaction	sa;
 
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = waiter;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
 	pid = getpid();
-	printf("%d\n", pid);
-//	while (1)
-//	{
-//		sleep(20);
-//		if (input == 'x')
-//			break ;
-//		write(0, &input, 1);
-//		printf("%c\n", input);
-//	}
-//	write(1, &pid, 5);
+	printf("pid:%d\n", pid);
+	pause();
 }
