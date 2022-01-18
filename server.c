ade400a0e53169ee10d922ec3fdc65d6b4f4c958
Tiago Pinto Monteiro <tpinto-m@student.42lausanne.ch>
2022-01-18 14:53:53 +0100

    update receiver in bitwise

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: tpinto-m <marvin@24lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 14:22:08 by tpinto-m          #+#    #+#             */
-/*   Updated: 2022/01/17 14:05:38 by tpinto-m         ###   ########.fr       */
+/*   Updated: 2022/01/18 14:49:35 by tpinto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,22 @@ static void	send_ok(pid_t pid)
 
 static void	receiver(int signum, siginfo_t *info, void *context)
 {
-	static int	bit = 128;
-	static int	i = 0;
+	static int	bit = 8;
 	static char	c = 0;
 
 	(void)context;
+	bit--;
 	if (signum == SIGUSR1)
-	{
-		c += bit;
-		bit /= 2;
-	}
+		c = c | (1 << bit);
 	else if (signum == SIGUSR2)
-		bit /= 2;
-	if (++i == 8)
+		c = c & ~(1 << bit);
+	if (!bit)
 	{
 		if (c == 6)
 			send_ok(info->si_pid);
 		else
 			write(1, &c, 1);
-		bit = 128;
-		i = 0;
-		c = 0;
+		bit = 8;
 	}
 }
 
